6a0f2b14461822b4fdd59206c1d4aae711e33f37
Justin Winger <justinwinger4@gmail.com>
2020-04-07 23:04:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,9 +9,9 @@ class Song
 
   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
+  # def initialize
+  #   @@songs << self
+  # end
 
   def self.find_by_name(name)
     @@songs.detect{|a| a.name == name}
